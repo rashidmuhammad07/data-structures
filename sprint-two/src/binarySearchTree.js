2ca4eaae45fdaa80536715527dc4c9d4ac67b7cb
David Kumarbai <cygnidavid@gmail.com>
2017-10-14 08:57:57 -0700

    done

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,48 @@
 var BinarySearchTree = function(value) {
+  var tree = Object.create(BinaryTreeMethods);
+  tree.value = value;
+  tree.left = null;
+  tree.right = null;
+  return tree;
+};
+
+var BinaryTreeMethods = {};
+
+BinaryTreeMethods.insert = function(value) {
+  if (this.value > value) {
+    if (this.left) {
+      this.left.insert(value);
+    } else {
+      this.left = BinarySearchTree(value);
+    }
+  } else {
+    if (this.right) {
+      this.right.insert(value);
+    } else {
+      this.right = BinarySearchTree(value);
+    }
+  }
+};
+
+BinaryTreeMethods.contains = function(value) {
+  if (this.value === value) return true;
+  
+  var flag = false;
+  
+  if (this.value > value) {
+    if (this.left) flag = this.left.contains(value);
+  } else {
+    if (this.right) flag = this.right.contains(value);
+  }
+  
+  return flag;
 };
 
 
-/*
- * Complexity: What is the time complexity of the above functions?
- */
+
+BinaryTreeMethods.depthFirstLog = function(callback) {
+	callback(this.value);	
+	if (this.left) this.left.depthFirstLog(callback);
+	if (this.right) this.right.depthFirstLog(callback);
+	
+};
